a00c3263ba618662fa1d63d6fb6f19d30a57b58a
k0st4dev <veljko.kosta12@gmail.com>
2022-07-15 00:03:12 +0200

    first commit

---
+++ src/Request.js
@@ -0,0 +1,12 @@
+const key = "e80f950b0fdb6f59330eb0ef3317acae";
+
+const requests = {
+  requestPopular: `
+https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestHorror: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
